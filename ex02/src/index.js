af05e7ffc19892fa691e4155b4205f927bdee786
VuduPopaj <adnanduljkovic@hotmail.com>
2021-03-10 19:23:57 +0100

    Added ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main () {
+    //   Adnan
+    /*Rock and roll (often written as rock & roll, rock 'n' roll, or rock 'n roll) 
+    is a genre of popular music that evolved in the United States 
+    during the late 1940s and early 1950s.*/
+    
+
+
+}
+console.log(main());
+
+module.exports = main;
